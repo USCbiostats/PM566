6a7502fff6aa6d390dbf0a4c25f9425cfe81898d
George G. Vega Yon <g.vegayon@gmail.com>
2020-07-06 13:18:22 -0700

    Adding more examples with parallel

--- static/slides/10-hpc/sars-cov2-circle.R
+++ static/slides/10-hpc/sars-cov2-circle.R
@@ -13,13 +13,10 @@ probs_susc[2, 1] <- .5
 # Initializes the simulation
 init <- function(nsick, nwear, pop_size, nsteps) {
   
-  # Parameters
-  nc <- sqrt(pop_size)
-  nr <- nc
-  
-  status      <- matrix(0, nrow = nr, ncol = nc)
+  status      <- integer(pop_size)
   status_prev <- status
-  wears       <- matrix(FALSE, nrow = nr, ncol = nc)
+  wears       <- logical(pop_size)
+  wears[]     <- FALSE
   
   # Sampling infected and which wears/doesn't wear a mask
   status_prev[sample.int(n = pop_size, size = nsick, replace = FALSE)] <- codes["infected"]
@@ -37,67 +34,42 @@ init <- function(nsick, nwear, pop_size, nsteps) {
 }
 
 # Function to figure out the 8 neighbors of any given coordinate
-get_neighbors <- function(i, j, data.) {
+get_neighbors <- function(i, data.) {
   
-  left <- cbind(
-    if (i == 1) c(data.$nr, 1, 2)
-    else if (i == data.$nr) c(data.$nr - 1, data.$nr, 1)
-    else (i - 1 + 0:2),
-    if (j == 1) rep(data.$nc, 3)
-    else rep(j - 1, 3)
-  )
+  left <- if (i == 1) data.$pop_size
+    else {i - 1}
   
-  middle <- cbind(
-    if (i == 1) c(data.$nr, 2)
-    else if (i == data.$nr) c(i - 1, 1)
-    else c(i - 1, i + 1),
-    rep(j, 2)
-  )
+  right <- if (i == data.$pop_size) 1L
+    else {i + 1}
   
-  right <- cbind(
-    if (i == 1) c(data.$nr, 1, 2)
-    else if (i == data.$nr) c(data.$nr - 1, data.$nr, 1)
-    else (i - 1 + 0:2),
-    if (j == data.$nc) rep(1, 3)
-    else rep(j + 1, 3)
-  )
-  
-  rbind(left, middle, right)
+  c(left, right)
 }
 
 # Test
 dat <- init(10, 100, 100, 100)
-get_neighbors(1,1,dat)
-get_neighbors(4,4,dat)
-get_neighbors(2,2,dat)
+get_neighbors(1,dat)
+get_neighbors(100,dat)
+get_neighbors(4,dat)
+get_neighbors(2,dat)
 
-# # We can save time by pre-computing neighbors
-# neighbors <- vector("list", n)
-# counter <- 1L
-# for (i in 1:nr)
-#   for (j in 1:nc) {
-#     neighbors[[counter]] <- get_neighbors(i, j)
-#     counter <- counter + 1L
-#   }
-
-update_status <- function(i, j, data.) {
+update_status <- function(i, data.) {
   
   # Diseased and recovered have no effect on the model
-  if (data.$status_prev[i, j] %in% codes[c("deceased", "recovered")])
+  if (data.$status_prev[i] %in% codes[c("deceased", "recovered")])
     return()
   
   # Is it getting better?
-  if (data.$status_prev[i, j] == codes["infected"]) {
+  if (data.$status_prev[i] == codes["infected"]) {
     
-    data.$status[i,j] <- sample(codes[-2], 1, prob = probs_sick)
+    data.$status[i] <- sample(codes[-2], 1, prob = probs_sick)
     
   } else {
     
     # Getting the neighbors
-    local <- get_neighbors(i,j,data.)
+    local <- get_neighbors(i, data.)
     
     # Each infected neighbor is an independent chance of getting infected
-    local <- local[which(data.$status_prev[local] == codes["infected"]), drop=FALSE]
+    local <- local[which(data.$status_prev[local] == codes["infected"])]
     
     # No neighbor infected, nothing happens
     if (length(local) == 0L)
@@ -105,8 +77,8 @@ update_status <- function(i, j, data.) {
     
     # Otherwise, depending on whether they wear or not, there will be different
     # probabilities of getting the disease.
-    p <- probs_susc[cbind(data.$wears[i,j], data.$wears[local]) + 1L]
-    data.$status[i,j] <- any(runif(length(p)) < p)
+    p <- probs_susc[cbind(data.$wears[i], data.$wears[local]) + 1L]
+    data.$status[i] <- any(runif(length(p)) < p)
     
   }
   
@@ -127,9 +99,8 @@ update_status_all <- function(data.) {
   
   data.$current_step <- data.$current_step + 1L
   
-  for (i in 1L:data.$nr)
-    for (j in 1L:data.$nc)
-      update_status(i, j, data.)
+  for (i in 1L:data.$pop_size)
+      update_status(i, data.)
   
   # Recording the new state, and updating the statistics
   data.$status_prev <- data.$status
@@ -150,3 +121,9 @@ simulate_covid <- function(pop_size, nsick, nwears_mask, nsteps) {
   return(dat)
   
 }
+
+# ans <- replicate(
+#   1000, {
+#     simulate_covid(100, nsick = 10, nwears_mask = 0, 20)$statistics
+#   })
+# 
