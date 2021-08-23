6ed3f41c866d375bbfa8bd99dee9e8f092ba0723
George G Vega Yon <g.vegayon@gmail.com>
2021-08-23 11:17:10 -0700

    Extending list for site building

---
+++ website/build.R
@@ -0,0 +1,13 @@
+library(blogdown)
+
+# Listing things to build
+things <- list.files("content/class", pattern = "*Rmd", full.names = TRUE
+
+
+things <- c(things, list.files("content/reading", pattern = "*Rmd", full.names = TRUE))
+
+things <- c(things, path.expand("content/syllabus/index.Rmd"))
+
+
+build_site(build_rmd = filter_timestamp(things))
+
