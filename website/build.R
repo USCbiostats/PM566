a0911643dc01ad6f57c506995d7595d008a88dfb
George G Vega Yon <g.vegayon@gmail.com>
2021-09-10 19:04:55 -0700

    Adding libs back

--- website/build.R
+++ website/build.R
@@ -3,7 +3,6 @@ library(blogdown)
 # Listing things to build
 things <- list.files("content/class", pattern = "*Rmd", full.names = TRUE)
 
-
 things <- c(things, list.files("content/reading", pattern = "*Rmd", full.names = TRUE))
 
 things <- c(things, path.expand("content/syllabus/index.Rmd"))
